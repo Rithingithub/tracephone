26f4f5fcf22d8cccc1950226f2598fe8d16dc60e
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2021-11-19 20:32:25 +0530

    Update tracenumber.py

--- tracenumber.py
+++ tracenumber.py
@@ -3,9 +3,18 @@ from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
 
+import subprocess
+import sys
+
+try:
+    import phonenumbers
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'phonenumbers'])
+finally:
+    import phonenumbers
+
 number = input("number with country code =")
 phone1numbers = phonenumbers.parse(number)
 print(geocoder.description_for_number(phone1numbers, 'en'))
 print(carrier.name_for_number(phone1numbers, 'en'))
 print(timezone.time_zones_for_number(phone1numbers))
-
